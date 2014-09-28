f1cf7bb1e195e906f20723629bcc908dfc63fe42
Yu Shijia <scarlettyushijia@gmail.com>
2014-09-28 22:11:24 +0800

    print error messsage in delete() in ItemList

--- src/todo/model/testTemp.java
+++ src/todo/model/testTemp.java
@@ -8,16 +8,18 @@ public class testTemp {
 		
 		ItemList myList = new ItemList();
 		myList.add(new Item("basketball training", new DateTime(new Date(1411876800000L))));
+		//myList.add(new Item("Going for lab session"));
 		myList.add(new Item("attend meeting", new DateTime(new Date(1398916800000L))));
 		myList.add(new Item("finish homework"));
 		myList.add(new Item("Doing X", new DateTime(new Date(1404273600000L))));
+		
 
 		
 		
 		myList.displayList();
 		
-		myList.delete(2);
-		//myList.sortByTimeDecreasing();
+	//	myList.delete(2);
+		myList.sortByTimeDecreasing();
 		myList.displayList();
 
 		
