1a492aec3867862f9fd5e950b1003753c0710cc1
scottyzm <scott.yapzm@gmail.com>
2014-09-28 16:48:12 +0800

    Updated Item class TestCases

--- src/todo/model/ItemTest.java
+++ src/todo/model/ItemTest.java
@@ -1,53 +1,50 @@
 package todo.model;
 
 import static org.junit.Assert.*;
-
 import java.util.ArrayList;
-
+import java.util.Date;
 import org.junit.Test;
-
 public class ItemTest {
-	String item1 = "firstitem";
-	String datetime1 = "friday fifth october";
-	String item2 = "2nditem";
-	String datetime2 = "saturday sixth october";
-	String item3 = "3rditem";
-	String datetime3 = "sunday fifth october";
+	String item1 = "item-1";
+	String item2 = "item-2";
+	String item3 = "item-3";
+	Date currentDate1 = new Date();
+	DateTime testDateTime1 = new DateTime(currentDate1);
+	Item abc = new Item (item1,testDateTime1);
 	
-	/*
+	Date currentDate2 = new Date();
+	DateTime testDateTime2 = new DateTime(currentDate2);
+	Date currentDate3 = new Date();
+	DateTime testDateTime3 = new DateTime(currentDate3);
+	
 	@Test
-	public void testQtyMultiple() {
-		Item abc = new Item(item1,datetime1);
-		assertEquals(1,Item.getItemQty());
-		Item def = new Item(item2,datetime2);
-		assertEquals(2,Item.getItemQty());
-		Item ghi = new Item(item3,datetime3);
-		assertEquals(3,Item.getItemQty());
+	public void testItemConstructor() {
+		Item def = new Item (item1,testDateTime1);
+		System.out.println(Item.getItemQty());
+		assertEquals(abc.getDescription(),def.getDescription());
+		assertEquals(abc.getStartDateTime(),def.getStartDateTime());
+		assertEquals(Item.getItemQty(),2);
 		
 	}
+	
 	@Test
-	public void testDataInput() {
-		Item abc = new Item(item1,datetime1);
-		assertEquals(abc.getDescription(),item1);
-		assertEquals(abc.getStartDateTime(),datetime1);
-		assertEquals(abc.getItemId(),1);
-		assertEquals(abc.getLastItemId(),0);
-		assertEquals(5,Item.getItemQty());
+	public void testStaticAttributes() {
+		System.out.println(Item.getItemQty());
+		Item ghi = new Item(item3,testDateTime3);
+		System.out.println(Item.getItemQty());
+		assertEquals(Item.getItemQty(),3);
+		assertEquals(Item.getLastItemId(),3);
 	}
+	
+	
 	@Test
 	public void testTagsInput() {
 		ArrayList<String> tagList = new ArrayList<String>();
 		tagList.add("homework");
 		tagList.add("school");
-		Item abc = new Item(item1,datetime1);
 		abc.setTags(tagList);
 		assertEquals(tagList,abc.getTags());
 		abc.displayTagList();
 	}
-	@Test 
-	public void testDisaplayTags() {
-		
-	}
-	*/
 	
 }
