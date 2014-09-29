048378c231085fbf2bd62f6406a4c005fd4e9e81
Siwei <liusiwei.yjlo@gmail.com>
2014-09-29 17:16:11 +0800

    extract string utils from NLP

--- src/todo/library/NLP.java
+++ src/todo/library/NLP.java
@@ -1,7 +1,5 @@
 package todo.library;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
@@ -9,7 +7,6 @@ import java.util.List;
 
 import todo.model.DateTime;
 import todo.model.Item;
-import todo.model.ItemList;
 
 import com.joestelmach.natty.DateGroup;
 import com.joestelmach.natty.Parser;
@@ -34,14 +31,14 @@ public class NLP {
 		DateTime startDateTime = null;
 		DateTime dueDateTime = null;
 		
-		String msgToDetectDate = removeQuoted(msg);
+		String msgToDetectDate = StringUtil.removeQuoted(msg);
 		groups = parser.parse(msgToDetectDate);
 		
 		
 		// escape wrong date time parse by Natty
 		// skip when the date text is an integer
 		// or a word in the filterOut list
-		while (groups.size() > 0 && (isInteger(groups.get(0).getText())
+		while (groups.size() > 0 && (StringUtil.isInteger(groups.get(0).getText())
 				|| Arrays.asList(filterOut).contains(groups.get(0).getText()))){
 			 msgToDetectDate = msgToDetectDate.replace(groups.get(0).getText(), "");
 			if(msgToDetectDate.equals("")){
@@ -53,11 +50,11 @@ public class NLP {
 		
 		//escape when the first integer of the date/time is a part of the description
 		//if the first char is an integer, and there is no space before it
-		if(groups.size() > 0 && isInteger(groups.get(0).getText().charAt(0)+"") 
+		if(groups.size() > 0 && StringUtil.isInteger(groups.get(0).getText().charAt(0)+"") 
 				&& msgToDetectDate.charAt(msgToDetectDate.indexOf(groups.get(0).getText())-1) != ' '){
 			msgToDetectDate = groups.get(0).getText().substring(1);
 			//delete all the subsequent integers
-			while (isInteger(msgToDetectDate.charAt(0)+"")){
+			while (StringUtil.isInteger(msgToDetectDate.charAt(0)+"")){
 				msgToDetectDate = msgToDetectDate.substring(1);
 			}
 			groups = parser.parse(msgToDetectDate);
@@ -73,18 +70,18 @@ public class NLP {
 				dueDate = group.getDates().get(1);
 				syntaxTree = group.getSyntaxTree().toStringTree();
 				String[] syntaxTreeArray = syntaxTree.split("DATE_TIME ");
-				if(stringContainSub(syntaxTreeArray[1], timeKeyword)){
+				if(StringUtil.stringContainSub(syntaxTreeArray[1], timeKeyword)){
 					// if start date has time
 					startDateHasTime = true;
 				}
-				if(stringContainSub(syntaxTreeArray[2], timeKeyword)){
+				if(StringUtil.stringContainSub(syntaxTreeArray[2], timeKeyword)){
 					// if due date has time
 					dueDateHasTime = true;
 				}
 			}else if(group.getDates().size() == 1){
 				// only start time or due time
 				syntaxTree = group.getSyntaxTree().toStringTree();
-				if(stringContainSub(syntaxTree, timeKeyword)){
+				if(StringUtil.stringContainSub(syntaxTree, timeKeyword)){
 					// if has time
 					startDateHasTime = true;
 				}
@@ -92,7 +89,7 @@ public class NLP {
 			}
 			
 			// delete preposition before date
-			String wordBeforeDate = getWordBeforeSubstring(msg,group.getText());
+			String wordBeforeDate = StringUtil.getWordBeforeSubstring(msg,group.getText());
 			if (Arrays.asList(preStart).contains(wordBeforeDate)
 					|| Arrays.asList(preDue).contains(wordBeforeDate)){
 				// if it is due date, then set to date2
@@ -108,15 +105,15 @@ public class NLP {
 		}
 		
 		// find long location
-		String locationString = getBracketLocation(msg);
+		String locationString = StringUtil.getBracketLocation(msg);
 		if (locationString.length() > 0){
 			location = locationString.substring(2, locationString.length()-1);
 			msg = msg.replace(locationString, "");
-			msg = trimString(msg);
+			msg = StringUtil.trimString(msg);
 		}
 
 		// find out one word location and all the tags
-		msg = trimString(msg);
+		msg = StringUtil.trimString(msg);
 		strArray = msg.split(" ");
 		for(int i = strArray.length-1; i >= 0 ; i--){
 			if (strArray[i].length() > 1 && strArray[i].charAt(0) == '#'){
@@ -134,7 +131,7 @@ public class NLP {
 		msg = msg.replaceAll("\\\\@", "@");
 		
 		//if the whole sentence is quoted, then delete the quotation marks
-		msg = trimString(removeFullQuote(msg));
+		msg = StringUtil.trimString(StringUtil.removeFullQuote(msg));
 		
 		//print out for testing
 		/*
@@ -160,131 +157,5 @@ public class NLP {
 		return new Item(msg, startDateTime, dueDateTime, location, 1, tagList);
 	}
 	
-	private static String trimString(String str){
-		return str.trim().replaceAll(" +", " ");
-	}
-	
-	/**
-	 * Get the word before the given substring
-	 * @param str original string
-	 * @param sub substring
-	 * @return the word string before the substring
-	 */
-	private static String getWordBeforeSubstring(String str, String sub){
-		int idx = str.lastIndexOf(sub)-2;
-		String result = "";
-		while (idx > 0 && str.charAt(idx) != ' '){
-			result = str.charAt(idx) + result;
-			idx--;
-		}
-		return result;
-	}
-	
-	/**
-	 * Check if a string can be converted into an integer
-	 * @param s string
-	 * @return boolean
-	 */
-	private static boolean isInteger(String s) {
-	    try { 
-	        Integer.parseInt(s); 
-	    } catch(NumberFormatException e) { 
-	        return false; 
-	    }
-	    return true;
-	}
-	
-	/**
-	 * Delete all the content quoted in a string
-	 * @param str original string
-	 * @return cleaned string
-	 */
-	private static String removeQuoted(String str){
-		//System.out.println("current str: "+str);
-		if(str.contains("\"") && str.replaceAll("[^\"]", "").length() % 2 == 0) {
-			//there is quotation, and the number of them is even
-			String toDelete = "\"";
-			int idx = str.indexOf('"');
-			while(idx+1<str.length() && str.charAt(idx+1) != ('\"')){
-				toDelete += str.charAt(idx+1);
-				idx++;
-			}
-			if(idx+1<str.length() && str.charAt(idx+1) == '\"'){
-				toDelete += "\"";
-				str = str.replace(toDelete, "");
-			}
-			return removeQuoted(str).trim().replaceAll(" +", " ");
-		}else{
-			return str.trim().replaceAll(" +", " ");
-		}
-	}
-	
-	/**
-	 * if there are only two quotation marks in the string
-	 * and one is at the beginning, and the other is at the end
-	 * then remove the two quotation marks
-	 * @param str the original string
-	 * @return string
-	 */
-	private static String removeFullQuote(String str){
-		if (str.length() > 1){
-			if (str.charAt(0) == '\"' && str.charAt(str.length()-1) == '\"'){
-				int count = 0;
-				for(int i = 0; i < str.length(); i++){
-					if(str.charAt(i) == '\"'){
-						count++;
-					}
-				}
-				if (count == 2){
-					return str.substring(1, str.length()-1);
-				}
-			}
-		}
-		return str;
-	}
-	
-	/**
-	 * get the location within a pair of brackets after @
-	 * @param str original string
-	 * @return "@(content)"
-	 */
-	private static String getBracketLocation(String str){
-		String toDelete = "";
-		if(str.contains("@(")) {
-			toDelete = "@(";
-			int idx = str.indexOf("@(");
-			
-			// if there is an escape character
-			if (str.charAt(idx-1) == '\\'){
-				return getBracketLocation(str.substring(idx+1, str.length()));
-			}
-			idx++;
-			while(idx+1<str.length() && str.charAt(idx+1) != (')')){
-				toDelete += str.charAt(idx+1);
-				idx++;
-			}
-			if(idx+1<str.length() && str.charAt(idx+1) == ')'){
-				toDelete += ")";
-			}else{
-				toDelete = "";
-			}
-		}
-		return toDelete;
-	}
-	
-	/**
-	 * This method check a string contains any string in a string array
-	 * @param str the string to be checked
-	 * @param list a string array
-	 * @return boolean
-	 */
-	private static boolean stringContainSub(String str, String[] list){
-		for (int i = 0; i < list.length; i++){
-			if (str.contains(list[i])){
-				return true;
-			}
-		}
-		return false;
-	}
 
 }
---
+++ src/todo/library/StringUtil.java
@@ -0,0 +1,134 @@
+package todo.library;
+
+public class StringUtil {
+
+	/**
+	 * This method check a string contains any string in a string array
+	 * @param str the string to be checked
+	 * @param list a string array
+	 * @return boolean
+	 */
+	public static boolean stringContainSub(String str, String[] list){
+		for (int i = 0; i < list.length; i++){
+			if (str.contains(list[i])){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * Delete all the content quoted in a string
+	 * @param str original string
+	 * @return cleaned string
+	 */
+	public static String removeQuoted(String str){
+		//System.out.println("current str: "+str);
+		if(str.contains("\"") && str.replaceAll("[^\"]", "").length() % 2 == 0) {
+			//there is quotation, and the number of them is even
+			String toDelete = "\"";
+			int idx = str.indexOf('"');
+			while(idx+1<str.length() && str.charAt(idx+1) != ('\"')){
+				toDelete += str.charAt(idx+1);
+				idx++;
+			}
+			if(idx+1<str.length() && str.charAt(idx+1) == '\"'){
+				toDelete += "\"";
+				str = str.replace(toDelete, "");
+			}
+			return removeQuoted(str).trim().replaceAll(" +", " ");
+		}else{
+			return str.trim().replaceAll(" +", " ");
+		}
+	}
+	
+	/**
+	 * Check if a string can be converted into an integer
+	 * @param s string
+	 * @return boolean
+	 */
+	public static boolean isInteger(String s) {
+	    try { 
+	        Integer.parseInt(s); 
+	    } catch(NumberFormatException e) { 
+	        return false; 
+	    }
+	    return true;
+	}
+	
+	/**
+	 * Get the word before the given substring
+	 * @param str original string
+	 * @param sub substring
+	 * @return the word string before the substring
+	 */
+	public static String getWordBeforeSubstring(String str, String sub){
+		int idx = str.lastIndexOf(sub)-2;
+		String result = "";
+		while (idx > 0 && str.charAt(idx) != ' '){
+			result = str.charAt(idx) + result;
+			idx--;
+		}
+		return result;
+	}
+	
+
+	
+	/**
+	 * if there are only two quotation marks in the string
+	 * and one is at the beginning, and the other is at the end
+	 * then remove the two quotation marks
+	 * @param str the original string
+	 * @return string
+	 */
+	public static String removeFullQuote(String str){
+		if (str.length() > 1){
+			if (str.charAt(0) == '\"' && str.charAt(str.length()-1) == '\"'){
+				int count = 0;
+				for(int i = 0; i < str.length(); i++){
+					if(str.charAt(i) == '\"'){
+						count++;
+					}
+				}
+				if (count == 2){
+					return str.substring(1, str.length()-1);
+				}
+			}
+		}
+		return str;
+	}
+	
+	/**
+	 * get the location within a pair of brackets after @
+	 * @param str original string
+	 * @return "@(content)"
+	 */
+	public static String getBracketLocation(String str){
+		String toDelete = "";
+		if(str.contains("@(")) {
+			toDelete = "@(";
+			int idx = str.indexOf("@(");
+			
+			// if there is an escape character
+			if (str.charAt(idx-1) == '\\'){
+				return getBracketLocation(str.substring(idx+1, str.length()));
+			}
+			idx++;
+			while(idx+1<str.length() && str.charAt(idx+1) != (')')){
+				toDelete += str.charAt(idx+1);
+				idx++;
+			}
+			if(idx+1<str.length() && str.charAt(idx+1) == ')'){
+				toDelete += ")";
+			}else{
+				toDelete = "";
+			}
+		}
+		return toDelete;
+	}
+	
+	public static String trimString(String str){
+		return str.trim().replaceAll(" +", " ");
+	}
+
+}
