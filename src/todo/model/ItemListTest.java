ce21e1a6f0a35dd43717ad7117f8d2d2d21b5e35
Yu Shijia <scarlettyushijia@gmail.com>
2014-10-16 01:57:18 +0800

    Test cases for ItemList
    test cases for size() method

---
+++ src/todo/model/ItemListTest.java
@@ -0,0 +1,33 @@
+package todo.model;
+
+import static org.junit.Assert.*;
+
+import java.util.Date;
+
+import org.junit.Test;
+
+public class ItemListTest {
+	String item1 = "item-1";
+	String item2 = "item-2";
+	String item3 = "item-3";
+	String item4 = "item-4";
+	
+	@Test
+	public void testSize() {
+		
+		ItemList myList = new ItemList();
+		
+		assertEquals(myList.size(), 0);
+		
+		Item itemA = new Item(item1);
+		myList.add(itemA);		
+		
+		assertEquals(myList.size(), 1);
+		
+		Item itemB = new Item(item2);
+		myList.add(itemB);
+		assertEquals(myList.size(), 2);
+		
+	}
+
+}
