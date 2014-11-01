c5a339d043b6c11cb8063bf39f0d2bdf96315b5f
yu yao <luiyuyao@hotmail.com>
2014-11-01 17:02:39 +0800

    Add GsonReader, GsonWriter and ItemStaticAttributes
    1. these class are yet to be integrated.

---
+++ src/todo/storage/GsonWriter.java
@@ -0,0 +1,31 @@
+package todo.storage;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+
+import todo.model.Item;
+import todo.model.ItemList;
+
+import com.google.gson.Gson;
+
+public class GsonWriter {
+	public GsonWriter() {
+
+	}
+
+	public void write(ItemList il) throws IOException {
+		Writer writer = new FileWriter("Output.json");
+
+		Gson gson = new Gson();
+		gson.toJson(il, writer);
+		
+		ItemStaticAttributes itemStaticAttributes = new ItemStaticAttributes();
+		itemStaticAttributes.setItemQty(Item.getItemQty());
+		itemStaticAttributes.setLastItemId(Item.getLastItemId());
+		
+		gson.toJson(itemStaticAttributes, writer);
+		
+		writer.close();
+	}
+}
---
+++ src/todo/storage/ItemStaticAttributes.java
@@ -0,0 +1,37 @@
+package todo.storage;
+
+import todo.model.Item;
+
+/**
+ * This is a class that maintains two Item's static attributes, which are itemQty and lastItemId.
+ * This class is used to cater for GSON's toJson and fromJson methods.
+ * @author Lui
+ *
+ */
+public class ItemStaticAttributes {
+	private int itemQty;
+	private int lastItemId;
+
+	public ItemStaticAttributes(){
+		itemQty = Item.getItemQty();
+		lastItemId = Item.getLastItemId();
+	}
+	
+	//getters
+	public int getItemQty() {
+		return this.itemQty;
+	}
+
+	public int getLastItemId() {
+		return this.lastItemId;
+	}
+
+	//setters
+	public void setItemQty(int itemQty){
+		this.itemQty = itemQty;
+	}
+	
+	public void setLastItemId(int lastItemId){
+		this.lastItemId = lastItemId;
+	}
+}
