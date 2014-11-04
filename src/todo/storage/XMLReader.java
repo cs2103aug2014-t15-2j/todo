949264461647e0657432ba052d0217996697386c
scottyzm <scott.yapzm@gmail.com>
2014-11-04 20:42:46 +0800

    Minor bug fixes and dateTime test cases

--- src/todo/storage/XMLReader.java
+++ src/todo/storage/XMLReader.java
@@ -74,7 +74,7 @@ public class XMLReader {
 			// SEVERE/WARNING/INFO/CONFIG/FINE/FINER/FINEST
 			LOGGER.setLevel(Level.INFO);
 			LOGGER.info("Error related to external file");
-			e.printStackTrace();
+			//e.printStackTrace();
 		}
 		
 		// There only one ITEM_QUANTITY to set for Item
